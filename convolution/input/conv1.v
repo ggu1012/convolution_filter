e61e69ef6b41b67b8123f94cd2ce228a9fb2dece
ggu1012 <ggu1012@naver.com>
2021-12-16 02:58:47 +0900

    identical result compared to python script

--- convolution/input/conv1.v
+++ convolution/input/conv1.v
@@ -9,10 +9,10 @@ module conv1 (
     output reg wdata_r
 );
 
-reg signed [15:0] mem [0:27][0:2][0:8];
+reg signed [31:0] mem [0:27][0:2][0:8];
 reg [4:0] row;
 reg [1:0] col;
-reg [2:0] cycle;
+reg [4:0] cycle;
 integer i;
 wire signed [287:0] mem_pixel;
 
@@ -21,6 +21,7 @@ wire [31:0] conv_result;
 
 
 kernel_pixel u0(
+    .clk(clk),
     .pixel(data_in),
     .weighted_pixel(mem_pixel)
 );
@@ -38,49 +39,81 @@ add_weighted u1(
     .conv_result(conv_result)
 );
 
-always@ (posedge clk or negedge reset_n) 
-begin
+always@ (negedge reset_n or posedge clk) begin
     if (!reset_n) begin
-        data_out <= 0; 
-        row <= -1;     
-    end  
-
-    else begin    
-        if (rdata_r == 1'b1) begin
-            row <= (row == 5'd27) ? 0 : row + 1;            
-            if (row >= 2 && col >=2) wdata_r <= 1;
-            else wdata_r <= 0;
-        end
+        row <= -1;
+        col <= 0;
+        cycle <= 0;
+        data_out <= 0;
     end
-    
+    else begin
+
+        row <= (row == 5'd27) ? 0 : row + 1;
+        // row, col, cycle counter
+        if (row == 5'd27) begin
+            row <= 0;
+            if (col == 2'd2) begin
+                col <= 0;
+                cycle <= cycle + 1;
+            end
+            else 
+                col <= col + 1;
+        end
+        else
+            row <= row + 1;  
+    end
 end
 
-always@(row, col) begin
-    if (row == 5'b0)
-        col <= (col == 2'd3)? 0 : col + 1;
-    if (col == 2'b0)
-        cycle <= cycle + 1;
-end
 
-always@ (posedge clk or negedge reset_n) begin
-    if (!reset_n) begin
-        col <= -1;
-        cycle <= -1;        
-    end       
+
+
+always@(*) begin   
+
+
+    // wdata_r signal
+    if (cycle == 0) begin
+        if (col == 2'd2)
+            wdata_r <= (row >= 2 && row <= 5'd27) ? 1 : 0;
+        else
+            wdata_r <= 0;         
+    end
+    else
+        wdata_r <= (row >= 2) ? 1 : 0;    
 
     if (rdata_r == 1'b1) begin
         for(i=0; i<9; i=i+1)
             mem[row][col][i] <= mem_pixel[32*i +: 32];  
-    end    
+    end 
+
+    if (wdata_r == 1) begin
+        if (col == 2'd2) begin
+            p00 <= mem[row-2][col-2][0]; p01 <= mem[row-1][col-2][1]; p02 <= mem[row][col-2][2];
+            p10 <= mem[row-2][col-1][3]; p11 <= mem[row-1][col-1][4]; p12 <= mem[row][col-1][5];
+            p20 <= mem[row-2][col][6]; p21 <= mem[row-1][col][7]; p22 <= mem[row][col][8];       
+        end
+        else if (col == 2'd0) begin            
+            p00 <= mem[row-2][col][6]; p01 <= mem[row-1][col][7]; p02 <= mem[row][col][8];
+            p10 <= mem[row-2][col+1][0]; p11 <= mem[row-1][col+1][1]; p12 <= mem[row][col+1][2];
+            p20 <= mem[row-2][col+2][3]; p21 <= mem[row-1][col+2][4]; p22 <= mem[row][col+2][5];                  
+        end
+        else if (col == 2'd1) begin
+            p00 <= mem[row-2][col-1][3]; p01 <= mem[row-1][col-1][4]; p02 <= mem[row][col-1][5];
+            p10 <= mem[row-2][col][6]; p11 <= mem[row-1][col][7]; p12 <= mem[row][col][8];
+            p20 <= mem[row-2][col+1][0]; p21 <= mem[row-1][col+1][1]; p22 <= mem[row][col+1][2];            
+        end
+    end
+
+
+    data_out <= conv_result;
+
 end
 
 endmodule
 
-
-
 module kernel_pixel(
+    input clk,
     input signed [15:0] pixel,
-    output signed [287:0] weighted_pixel
+    output reg signed [287:0] weighted_pixel
 );
 
 wire signed [15:0] weight [0:2][0:2];
@@ -98,7 +131,8 @@ generate
     genvar i, j;
     for(i=0; i<3; i=i+1) begin : weight_col
         for(j=0;j<3;j=j+1) begin : weight_row
-            assign weighted_pixel[96*i+32*(j+1)-1: 96*i+32*j] = weight[i][j] * pixel;
+            always@ (posedge clk)
+                weighted_pixel[96*i+32*(j+1)-1: 96*i+32*j] <= weight[i][j] * pixel;
         end
     end
 endgenerate
@@ -119,16 +153,21 @@ module add_weighted(
     output [31:0] conv_result
 );
 
-wire [31:0] tmp [0:6];
-wire [7:0] ov;
+wire signed [31:0] bias;
+
+wire [31:0] tmp [0:7];
+wire [8:0] ov;
+
+assign bias = -32'd58730196;
 
 csa32 u0(p0,p1,tmp[0], ov[0]);
 csa32 u1(p2,p3,tmp[1], ov[1]);
 csa32 u2(p4,p5,tmp[2], ov[2]);
 csa32 u3(p6,p7,tmp[3], ov[3]);
-csa32 u01(tmp[0], tmp[1], tmp[4], ov[4]);
-csa32 u23(tmp[1], tmp[2], tmp[5], ov[5]);
-csa32 u45(tmp[4], tmp[5], tmp[6], ov[6]);
-csa32 ufinal(tmp[6], p8, conv_result, ov[7]);
+csa32 u4(p8, bias, tmp[4], ov[4]);
+csa32 u5(tmp[0], tmp[1], tmp[5], ov[5]);
+csa32 u6(tmp[2], tmp[3], tmp[6], ov[6]);
+csa32 u7(tmp[5], tmp[6], tmp[7], ov[7]);
+csa32 u8(tmp[4], tmp[7], conv_result, ov[8]);
 
 endmodule
--- convolution/input/do_conv.v
+++ convolution/input/do_conv.v
@@ -5,6 +5,7 @@ module do_conv;
     wire signed [31:0] data_out;
     reg [15:0] data_in;
     reg rdata_r;
+    integer i;
 
 
     integer file_input [0:19];
@@ -14,7 +15,7 @@ module do_conv;
     conv1 k1(.data_in(data_in),
              .data_out(data_out),
              .clk(clk),
-             .resetn(rst),
+             .reset_n(rst),
              .rdata_r(rdata_r),
              .wdata_r(wdata_r)
             );
@@ -22,45 +23,101 @@ module do_conv;
 
     initial
     begin
-        clk <= 1'b0;
+        clk <= 1'b1;
+        rdata_r <= 1'b0;
+        rst <= 1'b1;
         forever
             #50 clk = ~clk;
     end
 
     initial
-    begin
-        #100
-         rst = 1'b1;
-        #100
-         rst = 1'b0;
+    begin       
+
+file_input[0] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_00.txt", "r"); 
+file_input[1] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_01.txt", "r");
+file_input[2] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_02.txt", "r");
+file_input[3] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_03.txt", "r");
+file_input[4] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_04.txt", "r");
+file_input[5] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_05.txt", "r");
+file_input[6] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_06.txt", "r");
+file_input[7] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_07.txt", "r");
+file_input[8] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_08.txt", "r");
+file_input[9] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_09.txt", "r");
+file_input[10] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_10.txt", "r");
+file_input[11] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_11.txt", "r");
+file_input[12] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_12.txt", "r");
+file_input[13] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_13.txt", "r");
+file_input[14] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_14.txt", "r");
+file_input[15] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_15.txt", "r");
+file_input[16] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_16.txt", "r");
+file_input[17] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_17.txt", "r");
+file_input[18] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_18.txt", "r");
+file_input[19] = $fopen("C:/altera/16.0/project/conv/convolution/batch/input_19.txt", "r");
+
+file_output[1] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_01.txt", "r");
+file_output[2] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_02.txt", "r");
+file_output[3] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_03.txt", "r");
+file_output[4] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_04.txt", "r");
+file_output[5] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_05.txt", "r");
+file_output[6] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_06.txt", "r");
+file_output[7] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_07.txt", "r");
+file_output[8] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_08.txt", "r");
+file_output[9] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_09.txt", "r");
+file_output[10] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_10.txt", "r");
+file_output[11] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_11.txt", "r");
+file_output[12] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_12.txt", "r");
+file_output[13] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_13.txt", "r");
+file_output[14] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_14.txt", "r");
+file_output[15] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_15.txt", "r");
+file_output[16] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_16.txt", "r");
+file_output[17] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_17.txt", "r");
+file_output[18] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_18.txt", "r");
+file_output[19] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_19.txt", "r");
 
 
-        file_input[0] = $fopen("../batch/input_00.txt", "r");
-        
+        for (i=0; i<20; i=i+1) begin
+            
 
-        rdata_r = 1;
+            file_output[0] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_00.txt", "r");
 
-        repeat (768) begin
-            #100 din = $fscanf(file_input[0], "%6d", data_in);
+            #50 rst = 1'b0;
+            #100 rst = 1'b1;
+            #50 rdata_r = 1;
+            repeat (784) begin
+                din = #100 $fscanf(file_input[i], "%6d", data_in);
+                if (wdata_r == 1)     
+                    $fdisplay(file_output[i], "%12d", data_out); 
+            end
+            rdata_r = 0;
+
+            file_output[0] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_00.txt", "a");
+
+            #50 rst = 1'b0;
+            #100 rst = 1'b1;
+            #50 rdata_r = 1;
+            repeat (784) begin
+                din = #100 $fscanf(file_input[i], "%6d", data_in);
+                if (wdata_r == 1)     
+                    $fdisplay(file_output[i], "%12d", data_out); 
+            end
+            rdata_r = 0;
+
+            file_output[0] = $fopen("C:/altera/16.0/project/conv/convolution/batch/output_00.txt", "a");
+            #50 rst = 1'b0;
+            #100 rst = 1'b1;
+            #50 rdata_r = 1;
+            repeat (784) begin
+                din = #100 $fscanf(file_input[i], "%6d", data_in);
+                if (wdata_r == 1)     
+                    $fdisplay(file_output[i], "%12d", data_out); 
+            end
+            rdata_r = 0;
+
+
+            $fclose(file_input[i]);
+            $fclose(file_output[i]);
+            $finish();
         end
-
-        rdata_r = 0;
-
-        
-
-
-        file_output[0] = $fopen("../batch/output_00.txt", "w");
-
-
-
-        repeat (676)
-            #100 $fdisplay(file_output[0], "%12d", data_out);
-
-
-
-        $fclose(file_input[0]);
-        // $fclose(file_output);
-        $finish();
 
     end
 
